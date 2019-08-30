c805c45b3573216bf5608c27a8eeddedde5bed2c
Denis Gamidov <dgamidov@gmail.com>
2019-08-30 16:14:56 +0300

    Refactor

--- algorithms/implementation/5-kangaroo.py
+++ algorithms/implementation/5-kangaroo.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 
 
-# Complete the kangaroo function below.
 def kangaroo(x1, v1, x2, v2):
+    if v1 > v2:
+        abs_speed = v1 - v2
+        abs_position = x2 - x1
 
-    if v2 >= v1:
-        return "NO"
-    else:
-        speed = v1 - v2
-        position = x2 - x1
-
-        if position % speed == 0:
+        if abs_position % abs_speed == 0:
             return "YES"
         else:
             return "NO"
+    else:
+        return "NO"
 
 print(kangaroo(0, 3, 4, 2))
 print(kangaroo(0, 2, 5, 3))
